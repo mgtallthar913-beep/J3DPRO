3107baf609180f274916237418af0f76daff1712
mgtallthar913-beep <mgtallthar913@gmail.com>
2026-02-28 06:21:41 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'next3d-v1';
+const urlsToCache = [
+  'index7.5.5.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
